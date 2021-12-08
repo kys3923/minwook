a60c3aace4ad78e95c9de7a557e962a7865cee4e
Youngsang Kim <youngsangkim@YoungsasMBP2021.home>
2021-12-08 14:18:27 -0500

    done for the chunk landing page, working on backend

--- src/pages/landing_parts/IntroAndOrder.js
+++ src/pages/landing_parts/IntroAndOrder.js
@@ -2,6 +2,8 @@ const IntroAndOrder = (props) => {
   return (
     <div className="IntroAndOrder_Container">
       <h4>IntroAndOrder Section</h4>
+      <button>Order Take Out</button>
+      <button>Make Reservation</button>
     </div>
   );
 }
