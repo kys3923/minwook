7d51216256943b04b8877adbe4da273f644d76c9
Youngsang Kim <youngsangkim@YoungsasMBP2021.home>
2021-12-08 03:06:36 -0500

    done for 12/08/21

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,12 @@
 import Maps from "./landing_parts/Maps";
 import Menu from "./landing_parts/Menu";
+import IntroAndOrder from "./landing_parts/IntroAndOrder";
 
 const Landing = (props) => {
   return (
     <div>
       <h1>this is Landing page</h1>
+      <IntroAndOrder />
       <Menu />
       <Maps />
     </div>
---
+++ src/pages/landing_parts/IntroAndOrder.js
@@ -0,0 +1,8 @@
+const IntroAndOrder = (props) => {
+  return (
+    <div className="IntroAndOrder_Container">
+      <h4>IntroAndOrder Section</h4>
+    </div>
+  );
+}
+export default IntroAndOrder;
